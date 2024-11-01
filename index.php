e92b336d603f96e9e674026d1519db3b44a4cbd8
David <davidrm901503@gmail.com>
2024-11-01 18:26:06 -0300

    refactorizando

--- index.php
+++ index.php
@@ -147,6 +147,102 @@
             display: block;
             margin: auto;
         }
+
+        #ready-to-take .flex-img {
+            background-image: url("img/sections/Careerpic.png");
+            background-size: cover;
+            background-position: center;
+            background-size: 100% 100%;
+        }
+
+        .align-txt {
+            padding-left: 80px;
+        }
+
+        #contact h2 {
+            font-size: 25px;
+            color: #4f87bb;
+        }
+
+        #contact h2 {
+            font-size: 25px;
+            color: #4f87bb;
+        }
+
+        #what-we-do h2 {
+            margin-bottom: 0;
+        }
+
+        .vertical-divider {
+            width: 1px;
+            height: 306px;
+            background-color: #ee8902;
+            display: inline-block;
+            padding: 1px;
+            position: relative;
+            top: 20px;
+        }
+
+        .logo-gris {
+            width: 435px;
+            height: 168px
+        }
+
+        .img-grupo1 {
+            width: 538px;
+            height: 352px
+        }
+
+        .img-grupo2 {
+            width: 538px;
+            height: 352px
+        }
+        .menu2{
+                display: none;
+            }
+
+        @media (max-width: 992px) {
+            .vertical-divider {
+                display: none;
+                /* Oculta la línea en pantallas pequeñas */
+            }
+        }
+
+        @media (max-width: 700px) {
+
+            .img-grupo1,
+            .img-grupo2,
+            .logo-gris {
+                width: 100%;
+                height: auto;
+            }
+
+            .grupos {
+                flex-direction: column;
+            }
+
+            .foter {
+                display: none;
+            }
+
+            .follow-div {
+                display: block;
+            }
+
+            .follow-div a {
+                color: white;
+                text-decoration: none;
+                display: block;
+                text-align: center;
+            }
+            .menu2{
+                display: block;
+            }
+            footer .menu2 a{
+                color: white;
+                text-decoration: none;
+            }
+        }
     </style>
     <title>Elementia USA</title>
 </head>
@@ -170,12 +266,15 @@
             <div class="row">
                 <div class="col-md-7">
                     <div class="content">
-                        <img src="img/logos/logo-gris.png" alt="Allura" style="width:435px; height: 168px">
-                        <p class="text-white mb-4">Elementia USA provides shared business consolidation services to U.S.-based subsidiaries of Elementia Materiales and Fortaleza Materiales.</p>
+                        <img class="logo-gris" src="img/logos/logo-gris.png" alt="Allura" style="">
+                        <p class="text-white mb-4 align-txt">Elementia USA provides shared business consolidation services to U.S.-based subsidiaries of Elementia Materiales and Fortaleza Materiales.</p>
 
                     </div>
                 </div>
                 <div class="col-md-5 follow-div">
+                    <a href="https://www.greatplacetowork.com/certified-company/7005415" target="_blank">
+                        <img class="w-100px" src="img/logos/great-work.png" alt="">
+                    </a>
                     <a target="_blank" href="https://www.linkedin.com/company/elementia-usa-inc./">Follow us on <img width="40" src="img/logos/linkedin.png" alt="Allura"></a>
                 </div>
             </div>
@@ -208,27 +307,28 @@
 
     <section id="logos">
         <div class="container ">
-            <div class="row flex">
-                <h5 class="text-center fw-bold mb-4">Meet our group</h5>
-                <div class="col">
-                    <img src="img/sections/grupo1.png" alt="" usemap="#image-map" style="width:538px; height:352px">
-                    <map name="image-map">
-                        <area target="_blank" alt="Fortaleza Materiales" href="https://www.fortalezamateriales.com/en.html" coords="413,6,118,103" shape="rect">
-                        <area target="_blank" alt="GCHI" href="https://www.gchi.com" coords="413,128,121,201" shape="rect">
-                        <area target="_blank" alt="Giant cement" href="https://www.giantcement.us/" coords="16,223,128,337" shape="rect">
-                        <area target="_blank" alt="Keystone Cement" href="https://www.keystonecement.us/" coords="145,224,257,338" shape="rect">
-                        <area target="_blank" alt="Dragon cement" href="https://www.dragoncement.us/" coords="273,222,387,335" shape="rect">
-                        <area target="_blank" alt="GRR" href="www.grr-gchi.us" coords="404,223,518,336" shape="rect">
-                    </map>
-                </div>
-                <div class="col">
-                    <img src="img/sections/grupo2.png" alt="" usemap="#image-map2" style="width:538px; height:352px">
-                    <map name="image-map2">
-                        <area shape="rect" href="https://www.elementiamateriales.com/en.html" alt="Elementia Materiales" coords="126,14,414,106" target="_blank" />
-                        <area shape="rect" href="https://allurausa.com/" alt="Allura" coords="25,250,269,320" target="_blank" />
-                        <area shape="rect" href="https://www.nacobreusa.com/" alt="Nacobre" coords="293,247,531,318" target="_blank" />
-                    </map>
-                </div>
+            <div class="row flex grupos">
+                <h2 class="text-center fw-bold  text-uppercase">Meet our group</h5>
+                    <div class="col">
+                        <img class="img-grupo1" src="img/sections/grupo1.png" alt="" usemap="#image-map">
+                        <map name="image-map">
+                            <area target="_blank" alt="Fortaleza Materiales" href="https://www.fortalezamateriales.com/en.html" coords="413,6,118,103" shape="rect">
+                            <area target="_blank" alt="GCHI" href="https://www.gchi.us" coords="413,128,121,201" shape="rect">
+                            <area target="_blank" alt="Giant cement" href="https://www.giantcement.us/" coords="16,223,128,337" shape="rect">
+                            <area target="_blank" alt="Keystone Cement" href="https://www.keystonecement.us/" coords="145,224,257,338" shape="rect">
+                            <area target="_blank" alt="Dragon cement" href="https://www.dragoncement.us/" coords="273,222,387,335" shape="rect">
+                            <area target="_blank" alt="GRR" href="https://www.grr-gchi.us/" coords="404,223,518,336" shape="rect">
+                        </map>
+                    </div>
+                    <div class="vertical-divider"></div>
+                    <div class="col">
+                        <img class="img-grupo2" src="img/sections/grupo2.png" alt="" usemap="#image-map2">
+                        <map name="image-map2">
+                            <area shape="rect" href="https://www.elementiamateriales.com/en.html" alt="Elementia Materiales" coords="126,14,414,106" target="_blank" />
+                            <area shape="rect" href="https://allurausa.com/" alt="Allura" coords="25,250,269,320" target="_blank" />
+                            <area shape="rect" href="https://www.nacobreusa.com/" alt="Nacobre" coords="293,247,531,318" target="_blank" />
+                        </map>
+                    </div>
             </div>
 
         </div>
@@ -237,15 +337,17 @@
     <section id="ready-to-take">
         <div class="row d-flex justify-content-between margin-0">
             <div class="col-md-6 flex-img great">
-                <img class="w-100px" src="img/logos/great-work.png" alt="">
+                <a href="https://www.greatplacetowork.com/certified-company/7005415" target="_blank">
+                    <img class="w-100px" src="img/logos/great-work.png" alt="">
+                </a>
             </div>
             <div class="col-md-6 flex-blue text-white d-flex justify-content-center">
                 <div>
                     <h4 class="fw-semibold text-uppercase text-center fs-1-1-rem">READY TO TAKE THE NEXT STEP IN YOUR CAREER IN ONE OF OUR COMPANIES?</h4>
                     <p class="mb-4 text-center">We are currently seeking talented individuals to join our dynamic team.</p>
                     <div class="text-center">
-                        <a href="https://workforcenow.adp.com/mascsr/default/mdf/recruitment/recruitment.html?cid=b5c5eb16-839b-46ec-a528-c40f4df252d0&ccId=9200448970359_2&lang=en_US" target="_blank" class="btn btn-red btn-lg">GCHI Careers</a>
-                        <a href="https://workforcenow.adp.com/mascsr/default/mdf/recruitment/recruitment.html?cid=04fcfc06-f798-4da9-a459-049fe296684b&ccId=19000101_000001&lang=en_US" target="_blank" class="btn btn-azul-claro btn-lg">Allura Careers</a>
+                        <a href="https://workforcenow.adp.com/mascsr/default/mdf/recruitment/recruitment.html?cid=04fcfc06-f798-4da9-a459-049fe296684b&ccId=19000101_000001&lang=en_US" target="_blank" class="btn btn-red btn-lg">GCHI Careers</a>
+                        <a href="https://workforcenow.adp.com/mascsr/default/mdf/recruitment/recruitment.html?cid=b5c5eb16-839b-46ec-a528-c40f4df252d0&ccId=9200448970359_2&lang=en_US" target="_blank" class="btn btn-azul-claro btn-lg">Allura Careers</a>
                     </div>
 
                 </div>
@@ -256,12 +358,8 @@
     <section id="what-we-do">
         <div class="container col-xxl-10 px-5">
             <div class="row text-center">
-                <div class="col-md-6">
+                <div class="col-md-12">
                     <h2 class="fw-semibold  text-uppercase">What can we do for you?</h2>
-
-                </div>
-                <div class="col-md-6 ">
-                    <a href="#contacto_form" class="btn btn-amarillo btn-lg">Contact us</a>
                 </div>
             </div>
         </div>
@@ -274,8 +372,8 @@
         <div class="container col-xxl-10 px-5">
             <div class="row">
                 <div class="col-md-12">
-                    <h6 class="fw-semibold text-center">Contact Us</h6>
-                    <h5 class="text-center fw-bold mb-5">Get in touch by filling out the form below.</h5>
+                    <h2 class="fw-semibold text-center">Contact Us</h6>
+                        <p class="text-center fw-bold mb-5">Get in touch by filling out the form below.</p>
                 </div>
                 <div class="col-md-10 offset-md-1">
                     <?php
@@ -393,7 +491,7 @@
         <div class="container col-xxl-10 px-5">
             <div class="d-flex justify-content-between">
                 <img width="150px" src="img/logos/logo-azul.png" alt="Allura">
-                <nav class="navbar py-3 bg-azul-oscuro">
+                <nav class="navbar py-3 bg-azul-oscuro foter">
                     <div class="menu">
                         <a href="https://investorcloud.s3.us-east-1.amazonaws.com/elementia_usa/Terms-of-Use.pdf" target="_blank">Terms of Use</a>
                         <a href="https://investorcloud.s3.us-east-1.amazonaws.com/elementia_usa/Privacy-Policy.pdf" target="_blank">Privacy Policy</a>
@@ -404,6 +502,15 @@
 
                     </div>
                 </nav>
+                <div class="menu2">
+                    <p><a href="https://investorcloud.s3.us-east-1.amazonaws.com/elementia_usa/Terms-of-Use.pdf" target="_blank">Terms of Use</a></p>
+                    <p> <a href="https://investorcloud.s3.us-east-1.amazonaws.com/elementia_usa/Privacy-Policy.pdf" target="_blank">Privacy Policy</a></p>
+                    <p><a class="cursor-normal" href="javascript:void(0)">© Copyright <script>
+                            document.write(new Date().getFullYear())
+                        </script>
+                    </a></p>
+
+                </div>
             </div>
         </div>
     </footer>
